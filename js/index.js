69dfbf8960decf7ea37fca41c75e1c5d64e5f94a
Brian Ly <Brian@Brians-MacBook-Air.local>
2017-08-25 22:40:09 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function closeNav(){
+  document.getElementById('slideout-menu').style.display = 'none'
+}
+
+function openNav(){
+  document.getElementById('slideout-menu').style.display = 'block'
+}
